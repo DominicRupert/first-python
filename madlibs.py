5ba4830063d78da9c844774541fab8312393c84d
DominicRupert <dominicrupert@gmail.com>
2022-08-16 21:20:20 -0600

    first commit

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+youtuber = "dominic rupert"
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+big_dick_programmer = input("big dick programmer: ")
+# print(f"subscribe to {youtuber}")
+madlib = f"Computer programming is {adj}! \
+i love to {verb1} and {verb2}. i want to be a {big_dick_programmer}"
+
+print(madlib)
